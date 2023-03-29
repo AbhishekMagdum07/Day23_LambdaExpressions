c62d02c9773526f1159e7472ba47f78a816e8028
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-29 14:50:16 +0530

    Added : User need to enter valid email address  otherwise throw custom exception

--- src/main/java/com/exception_handling/UserRagistration.java
+++ src/main/java/com/exception_handling/UserRagistration.java
@@ -23,5 +23,16 @@ public class UserRagistration {
             throw new InvalidUserInputException("Enter valid Last Name"); // throw exception if conditioon is false.
         }
     }
+    // Uc3 = As a User need to enter a valid email
+    //E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) wit precise @ and . positions
+    public boolean validEmail(String email) {
+        Pattern pattern = Pattern.compile("^[a-z][a-z0-9-.+]*@[a-z0-9]*[.][a-z.]*$");
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.find() == true) {
+            return matcher.matches();      // passing to test cases if condition is true.
+        } else {
+            throw new InvalidUserInputException("Enter valid Email Address"); // throw exception if conditioon is false.
+        }
+    }
 }
 
